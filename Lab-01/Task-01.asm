d6dd26a06ee849ec4104bfe85b22e0b3d472dc4e
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-05-06 16:26:22 +0600

    Update Task-01.asm

--- Lab-01/Task-01.asm
+++ Lab-01/Task-01.asm
@@ -6,15 +6,15 @@ RESULT DW ?
 .CODE
 
 START:  
-    MOV AX, 30
-    ADD AX, 15           
+    MOV AX, 30  // MOV diye kono variable store kori value exa: a=30 here AX=30
+    ADD AX, 15  // ADD AX,15 means a=a+15 mane a=30+15=45     
 
-    MOV BX, 575
-    SUB BX, 225          
+    MOV BX, 575 //MOV diye kono variable store kori value exa: b=575 here BX=575
+    SUB BX, 225    // SUB BX,225 means b=b-225 mane a=575-225=350     
 
-    MUL BX              
+    MUL BX         //MUL BX mane AXer value BX er Sathe multiply kori, maeans=AX*BX
 
-    ADD AX, 210         
-    MOV RESULT, AX     
+    ADD AX, 210  //ADD,AX 210 mane AX*BX+210       
+    MOV RESULT, AX     //MOV RESULT,AX Mane RESULT E AX ER value assign kori
 
-END START
+END START// PROGRAM END;
